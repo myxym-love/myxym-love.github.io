7e11dd0a11e5a665afae54d33f382b7c187577ae
myxym-love <1602271854@qq.com>
2022-05-03 15:58:28 +0800

    Site updated: 2022-05-03 15:58:27

--- js/matery.js
+++ js/matery.js
@@ -1,19 +1,6 @@
 $(function () {
 	
-	function switchNightMode() {
-        var body = document.body;
-        if(body.classList.contains('dark')){
-        document.body.classList.remove('dark');
-        localStorage.setItem('dark','0');
-        $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
-        return;
-        } else {
-        document.body.classList.add('dark');
-        localStorage.setItem('dark','1');
-        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
-        return;
-        }
-    }
+	
 	
     /**
      * 添加文章卡片hover效果.
